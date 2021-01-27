d6b9c021cb853e5d6fc7090453cd3be8d37a6bc4
Carlos Lubián <carlubian@outlook.es>
2021-01-27 12:16:17 +0100

    Allow log severities on helper methods

--- src/Serilog.Sinks.EventGrid/LoggerEventGridExtensions.cs
+++ src/Serilog.Sinks.EventGrid/LoggerEventGridExtensions.cs
@@ -1,60 +1,80 @@
 ﻿namespace Serilog.Sinks.EventGrid
 {
-  public static class LoggerEventGridExtensions
-  {
-    /// <summary>Information log event helper for EventGrid sink</summary>
-    /// <param name="logger">The Serilog ILogger</param>
-    /// <param name="eventType">The event type sent to EventGrid Grid</param>
-    /// <param name="subject">The event subject sent to EventGrid Grid</param>
-    /// <param name="messageTemplate">The Serilog logger message template</param>
-    /// <param name="props">The values references in the templace to be added to the EventGrid Grid data payload</param>
-    public static void Event(this ILogger logger, string eventType, string subject, string messageTemplate = "", params object[] props)
+    public static class LoggerEventGridExtensions
     {
-      if (!string.IsNullOrEmpty(subject))
-        logger = logger.ForContext("EventSubject", subject);
+        /// <summary>Debug log event helper for EventGrid sink</summary>
+        /// <param name="logger">The Serilog ILogger</param>
+        /// <param name="eventType">The event type sent to EventGrid Grid</param>
+        /// <param name="subject">The event subject sent to EventGrid Grid</param>
+        /// <param name="messageTemplate">The Serilog logger message template</param>
+        /// <param name="props">The values references in the templace to be added to the EventGrid Grid data payload</param>
+        public static void DebugEvent(this ILogger logger, string eventType, string subject, string messageTemplate = "", params object[] props)
+        {
+            logger = FixParams(logger, eventType, subject);
 
-      if (!string.IsNullOrEmpty(eventType))
-        logger = logger.ForContext("EventType", eventType);
+            logger.Debug(messageTemplate, props);
+        }
 
-      logger.Information(messageTemplate, props);
+        /// <summary>Information log event helper for EventGrid sink</summary>
+        /// <param name="logger">The Serilog ILogger</param>
+        /// <param name="eventType">The event type sent to EventGrid Grid</param>
+        /// <param name="subject">The event subject sent to EventGrid Grid</param>
+        /// <param name="messageTemplate">The Serilog logger message template</param>
+        /// <param name="props">The values references in the templace to be added to the EventGrid Grid data payload</param>
+        public static void InformationEvent(this ILogger logger, string eventType, string subject, string messageTemplate = "", params object[] props)
+        {
+            logger = FixParams(logger, eventType, subject);
+
+            logger.Information(messageTemplate, props);
+        }
+
+        /// <summary>Warning log event helper for EventGrid sink</summary>
+        /// <param name="logger">The Serilog ILogger</param>
+        /// <param name="eventType">The event type sent to EventGrid Grid</param>
+        /// <param name="subject">The event subject sent to EventGrid Grid</param>
+        /// <param name="messageTemplate">The Serilog logger message template</param>
+        /// <param name="props">The values references in the templace to be added to the EventGrid Grid data payload</param>
+        public static void WarningEvent(this ILogger logger, string eventType, string subject, string messageTemplate = "", params object[] props)
+        {
+            logger = FixParams(logger, eventType, subject);
+
+            logger.Warning(messageTemplate, props);
+        }
+
+        /// <summary>Error log event helper for EventGrid sink</summary>
+        /// <param name="logger">The Serilog ILogger</param>
+        /// <param name="eventType">The event type sent to EventGrid Grid</param>
+        /// <param name="subject">The event subject sent to EventGrid Grid</param>
+        /// <param name="messageTemplate">The Serilog logger message template</param>
+        /// <param name="props">The values references in the templace to be added to the EventGrid Grid data payload</param>
+        public static void ErrorEvent(this ILogger logger, string eventType, string subject, string messageTemplate = "", params object[] props)
+        {
+            logger = FixParams(logger, eventType, subject);
+
+            logger.Error(messageTemplate, props);
+        }
+
+        /// <summary>Fatal log event helper for EventGrid sink</summary>
+        /// <param name="logger">The Serilog ILogger</param>
+        /// <param name="eventType">The event type sent to EventGrid Grid</param>
+        /// <param name="subject">The event subject sent to EventGrid Grid</param>
+        /// <param name="messageTemplate">The Serilog logger message template</param>
+        /// <param name="props">The values references in the templace to be added to the EventGrid Grid data payload</param>
+        public static void FatalEvent(this ILogger logger, string eventType, string subject, string messageTemplate = "", params object[] props)
+        {
+            logger = FixParams(logger, eventType, subject);
+
+            logger.Fatal(messageTemplate, props);
+        }
+
+        private static ILogger FixParams(ILogger logger, string eventType, string subject)
+        {
+            if (!string.IsNullOrEmpty(subject))
+                logger = logger.ForContext("EventSubject", subject);
+
+            if (!string.IsNullOrEmpty(eventType))
+                logger = logger.ForContext("EventType", eventType);
+            return logger;
+        }
     }
-
-    /// <summary>Log to Event Grid with custom subject and message template w/ custom args</summary>
-    /// <param name="logger">The Serilog ILogger</param>
-    /// <param name="subject">The event subject sent to EventGrid Grid</param>
-    /// <param name="messageTemplate">The Serilog logger message template</param>
-    /// <param name="props">The values references in the templace to be added to the EventGrid Grid data payload</param>
-    public static void EventSubject(this ILogger logger, string subject, string messageTemplate, params object[] props)
-    {
-      logger.Event(null, subject, messageTemplate, props);
-    }
-
-    /// <summary>Log to Event Grid with custom type and message template</summary>
-    /// <param name="logger">The Serilog ILogger</param>
-    /// <param name="subject">The event subject sent to EventGrid Grid</param>
-    /// <param name="messageTemplate">The Serilog logger message template</param>
-    public static void EventSubject(this ILogger logger, string subject, string messageTemplate)
-    {
-      logger.Event(null, subject, messageTemplate, null);
-    }
-
-    /// <summary>Log to Event Grid with custom type and message template w/ custom args</summary>
-    /// <param name="logger">The Serilog ILogger</param>
-    /// <param name="eventType">The event type sent to EventGrid Grid</param>
-    /// <param name="messageTemplate">The Serilog logger message template</param>
-    /// <param name="props">The values references in the templace to be added to the EventGrid Grid data payload</param>
-    public static void EventType(this ILogger logger, string eventType, string messageTemplate, params object[] props)
-    {
-      logger.Event(eventType, null, messageTemplate, props);
-    }
-
-    /// <summary>Log to Event Grid with custom type and message template</summary>
-    /// <param name="logger">The Serilog ILogger</param>
-    /// <param name="eventType">The event type sent to EventGrid Grid</param>
-    /// <param name="messageTemplate">The Serilog logger message template</param>
-    public static void EventType(this ILogger logger, string eventType, string messageTemplate)
-    {
-      logger.Event(eventType, null, messageTemplate, null);
-    }
-  }
 }
